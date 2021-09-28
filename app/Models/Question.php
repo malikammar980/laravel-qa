909b9b6f3ced6e19e5704d34a2b79eaa66f18f27
Malik Ammar <ammar@allresourcetech.com>
2021-09-28 11:22:40 +0500

    add author info and question creation date

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -17,4 +17,10 @@ class Question extends Model
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str::slug($value);
     }
+    public function getUrlAttribute(){
+        return route("questions.show", $this->id);
+    }
+    public function getCreatedDateAttribute(){
+        return $this->created_at->diffForHumans();
+    }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -12,9 +12,14 @@
                             <div class="media">
                                 <div class="media-body">
                                     <h3 class="mt-0">
-                                        {{$question->title}}
+                                        <a href="{{$question->url}}">{{$question->title}}</a>
                                     </h3>
-                                    {{\Illuminate\Support\Str::limit($question->body, 250)}}
+                                    <p class="lead">
+                                        Asked by
+                                        <a href="{{$question->user->url}}">{{$question->user->name}}</a>
+                                        <small class="text-muted">{{$question->created_date}}</small>
+                                    </p>
+                                    {{\Illuminate\Support\Str::limit($question->body, 350)}}
                                 </div>
                             </div>
                             <hr>
