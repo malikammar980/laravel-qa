4aaf796a6b8ec2d9e2aa472629d7d3313f5f2349
Malik Ammar <ammar@allresourcetech.com>
2021-10-01 17:27:11 +0500

    Add vote controls to questions and answers items

---
+++ resources/js/fontawesome.js
@@ -0,0 +1,12 @@
+import fontawesome from '@fortawesome/fontawesome-free';
+// import faCaretUp from '@fortawesome/fontawesome-free/faCaretUp';
+import {faCaretUp} from '@fortawesome/free-solid-svg-icons/faCaretUp';
+import {faCaretDown} from '@fortawesome/free-solid-svg-icons/faCaretDown';
+import {faStar} from '@fortawesome/free-solid-svg-icons/faStar';
+import {faCheck} from '@fortawesome/free-solid-svg-icons/faCheck';
+
+
+
+
+fontawesome.library.add([faCaretUp, faCaretDown, faCheck, faStar]);
+
