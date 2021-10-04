0a94ac5c04b64396a6efa2aebd6fac7ac062b1b7
Malik Ammar <ammar@allresourcetech.com>
2021-10-04 16:47:33 +0500

    update best answer id upon deleteion of answer

--- resources/views/answers/_index.blade.php
+++ resources/views/answers/_index.blade.php
@@ -3,24 +3,22 @@
         <div class="card">
             <div class="card-body">
                 <div class="card-title">
-                    <h2>{{ $answersCount. " " . \Illuminate\Support\Str::plural('Answer', $answersCount) }}</h2>
+                    <h2>{{ $answersCount . " " . \Illuminate\Support\Str::plural('Answer', $answersCount) }}</h2>
                 </div>
                 <hr>
                 @include ('layouts._messages')
 
-                @foreach($answers as $answer)
+                @foreach ($answers as $answer)
                     <div class="media">
-                        <div class="d-flex flex-column vote-controls">
-                            <a title="This Answer is useful" href="" class="vote-up">
-                                <i class="fas fa-caret-up fa-4x"></i>
+                        <div class="d-fex flex-column vote-controls">
+                            <a title="This answer is useful" class="vote-up">
+                                <i class="fas fa-caret-up fa-3x"></i>
                             </a>
-                            <span class="votes-count">
-                                    5
-                                </span>
-                            <a title="This Answer isn't useful" href="" class="vote-down off">
-                                <i class="fas fa-caret-down fa-4x"></i>
+                            <span class="votes-count">1230</span>
+                            <a title="This answer is not useful" class="vote-down off">
+                                <i class="fas fa-caret-down fa-3x"></i>
                             </a>
-                            <a title="Mark this answer as best answer"  class="{{$answer->status}} mt-2">
+                            <a title="Mark this answer as best answer" class="{{ $answer->status }} mt-2">
                                 <i class="fas fa-check fa-2x"></i>
                             </a>
                         </div>
@@ -29,33 +27,30 @@
                             <div class="row">
                                 <div class="col-4">
                                     <div class="ml-auto">
-                                        @can('update', $answer)
-                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit </a>
+                                        @can ('update', $answer)
+                                            <a href="{{ route('questions.answers.edit', [$question->id, $answer->id]) }}" class="btn btn-sm btn-outline-info">Edit</a>
                                         @endcan
-                                        @can('delete', $answer)
-                                            <form action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}" method="POST" class="form-delete">
+                                        @can ('delete', $answer)
+                                            <form class="form-delete" method="post" action="{{ route('questions.answers.destroy', [$question->id, $answer->id]) }}">
                                                 @method('DELETE')
                                                 @csrf
-                                                <button class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure')">Delete</button>
+                                                <button type="submit" class="btn btn-sm btn-outline-danger" onclick="return confirm('Are you sure?')">Delete</button>
                                             </form>
                                         @endcan
                                     </div>
                                 </div>
+                                <div class="col-4"></div>
                                 <div class="col-4">
-
-                                </div>
-                                <div class="col-4">
-                                    <span class="text-muted">Answerd {{ $answer->created_date }}</span>
+                                    <span class="text-muted">Answered {{ $answer->created_date }}</span>
                                     <div class="media mt-2">
                                         <a href="{{ $answer->user->url }}" class="pr-2">
-                                            <img src="{{ $answer->user->avatar }}" alt="user">
+                                            <img src="{{ $answer->user->avatar }}">
                                         </a>
                                         <div class="media-body mt-1">
                                             <a href="{{ $answer->user->url }}">{{ $answer->user->name }}</a>
                                         </div>
-                                        </div>
+                                    </div>
                                 </div>
-
                             </div>
                         </div>
                     </div>
